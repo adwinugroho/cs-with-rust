7d5aaadbe163fc62314d816b59432d43eef8bcba
adwinugroho <adwinnugroho16@gmail.com>
2025-11-09 19:47:08 +0700

    feat: adjust basic information

--- basic_information/readme.md
+++ basic_information/readme.md
@@ -1,2 +1,12 @@
 ### Basic Information
-- 
+
+Apa itu informasi?
+- Representasi data dalam bentuk bit (0/1).
+- Ukuran: bit, byte, word.
+- Entropy, Shannon’s information theory.
+
+Contoh: 0xFF, 1 << 3, binary.Read.
+
+- Informasi itu bukan sekadar kata atau angka — tapi sesuatu yang mengurangi ketidakpastian.
+- Semakin tak terduga suatu kejadian, semakin banyak informasi yang dibawanya.
+- 1 Byte -> 8 Bit
--- basic_information/src/main.rs
+++ basic_information/src/main.rs
@@ -1,10 +1,12 @@
 fn main() {
-    println!("Cek bit!");
-    println!("koin adil: {:?}", info_bits(0.5));
-    println!("dadu 6 sisi: {:?}", info_bits(1.0 / 6.0));
+    println!("Cek bit! Jumlah informasi yang didapatkan dari:");
+    println!("koin adil (2 sisi): {:?}", info_bits(0.5)); // 1 bit = jumlah informasi untuk memilih antara dua kemungkinan yang sama besar.
+    println!("dadu 6 sisi: {:?}", info_bits(1.0 / 6.0)); // Artinya butuh 2,58 sekian untuk menyimpan hasil dari dadu 6 sisi
     println!("kejadian pasti: {:?}", info_bits(1.0));
 }
 
+// Jumlah informasi (bit) = log₂(1/p)
+// di mana p = probabilitas kejadian.
 fn info_bits(probability: f64) -> f64 {
     if probability == 0.0 || probability > 1.0 {
         return 0.0;
