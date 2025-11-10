a881b138bdc099f635adda59cce8c3e65272c91e
adwinugroho <adwinnugroho16@gmail.com>
2025-11-10 16:39:39 +0700

    feat: add example digital abstraction

--- digital_abstraction/src/main.rs
+++ digital_abstraction/src/main.rs
@@ -1,3 +1,26 @@
+const VIH: f64 = 2.5; // Voltage Input High
+const VOL: f64 = 0.5; // Voltage Output Low
+
 fn main() {
-    println!("Gerbang Logika~");
+    println!("Digital Abstraction dengan noise margin~");
+    let arr_voltages: [f64; 5] = [0.1, 0.4, 2.4, 2.6, 1.8]; // random voltage
+    for d in arr_voltages {
+        match digitalize(d) {
+            Ok(state) => println!("{:.1}V → {}", d, if state { "HIGH (1)" } else { "LOW (0)" }),
+            Err(e) => println!("{:.1}V → ⚠️  {}", d, e),
+        }
+    }
+}
+
+fn digitalize(voltage: f64) -> Result<bool, String> {
+    if voltage >= VIH {
+        return Ok(true);
+    } else if voltage <= VOL {
+        return Ok(false);
+    } else {
+        return Err(format!(
+            "Voltage {:.1}V berada di noise zone ({}V - {}V)",
+            voltage, VOL, VIH
+        ));
+    }
 }
