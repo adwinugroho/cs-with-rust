a21f086484c5f14a0dd317a4b9b8c7023546df98
adwinugroho <adwinnugroho16@gmail.com>
2025-11-10 11:28:06 +0700

    fix: remove .DS_Store & target folder

--- digital_abstraction/target/debug/deps/digital_abstraction-81a31e5c93ac95ca.d
+++
@@ -1,5 +0,0 @@
-/Users/adwinugroho/Documents/dev/self-learning/doing-rust/rust-journey/digital_abstraction/target/debug/deps/libdigital_abstraction-81a31e5c93ac95ca.rmeta: src/main.rs
-
-/Users/adwinugroho/Documents/dev/self-learning/doing-rust/rust-journey/digital_abstraction/target/debug/deps/digital_abstraction-81a31e5c93ac95ca.d: src/main.rs
-
-src/main.rs:
